527160d49ba8c841f9b31c8b8af57397b27654aa
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-09 01:06:20 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,16 @@
-
-const {createProxyMiddleware} = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
   let target = "https://github.com/";
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
 
+  // 创建代理对象
+  const proxy = createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    // 在这里可以添加其他代理配置
+  });
 
-
+  // 使用代理对象处理请求
   proxy(req, res, (err) => {
     if (err) {
       console.error("Error proxying request:", err);
@@ -43,20 +38,4 @@ module.exports = (req, res) => {
       };
     }
   });
-};
-
-
-
-
-
-  // 创建代理对象并转发请求
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/backend`
-      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-      //   "^/backend/": "/",
-    },
-  })(req, res);
 };
